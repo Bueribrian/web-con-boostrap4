ae34b0085b9b186f8186f7b5c77d4e6c4cf81619
bueribrian <bueribrian@hotmail.com>
2019-01-11 22:26:13 -0300

    header casi listo

---
+++ script.js
@@ -0,0 +1,27 @@
+window.onload = ()=>{
+    preloader.style.display='none'
+    
+}
+const navbar = document.querySelector('.navbar')
+window.addEventListener('scroll',()=>{
+    console.log(window.pageYOffset)
+    if(window.pageYOffset > navbar.offsetTop+120){
+        navbar.classList.remove('position-absolute')
+        navbar.classList.remove('bg-transparent')
+        navbar.classList.remove('navbar-transparent')
+        navbar.classList.add('fixed-top')
+        navbar.classList.add('bg-nb-black')
+       
+        console.log('fixed')
+    }else{
+        navbar.classList.add('position-absolute')
+        navbar.classList.add('bg-transparent')
+        navbar.classList.add('navbar-transparent')
+        navbar.classList.remove('fixed-top')
+        navbar.classList.remove('bg-nb-black')
+        
+        console.log('no-fixed')
+    }
+})
+
+const preloader = document.querySelector('.preloader-container')
