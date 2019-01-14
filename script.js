64812301c19be53e34826adce2eb372046e04741
bueribrian <bueribrian@hotmail.com>
2019-01-14 20:34:33 -0300

    casi listo 2

--- script.js
+++ script.js
@@ -3,24 +3,23 @@ window.onload = ()=>{
     
 }
 const navbar = document.querySelector('.navbar')
+console.log(navbar.offsetTop)
 window.addEventListener('scroll',()=>{
-    console.log(window.pageYOffset)
-    if(window.pageYOffset > navbar.offsetTop+50){
+    console.log(navbar.offsetTop)
+    if(window.pageYOffset > navbar.offsetTop+80){
         navbar.classList.remove('position-absolute')
         navbar.classList.remove('bg-transparent')
         navbar.classList.remove('navbar-transparent')
         navbar.classList.add('fixed-top')
         navbar.classList.add('bg-nb-black')
-       
-        console.log('fixed')
+        console.log(`Window offset = ${window.pageYOffset}
+                            navbar.offseTop = ${navbar.offsetTop}`)
     }else{
         navbar.classList.add('position-absolute')
         navbar.classList.add('bg-transparent')
         navbar.classList.add('navbar-transparent')
         navbar.classList.remove('fixed-top')
         navbar.classList.remove('bg-nb-black')
-        
-        console.log('no-fixed')
     }
 })
 
